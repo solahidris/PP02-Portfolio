c8d5d603cb1c4691f2f94b235f2841e595f13892
solahidris <emailsolah@gmail.com>
2023-06-16 20:06:37 +0800

    menu transition time

--- src/components/RightMenu.js
+++ src/components/RightMenu.js
@@ -71,10 +71,10 @@ const RightMenu = ( { scrollToSection } ) => {
 
       <Transition
         as={Fragment}
-        enter="transition ease-out duration-200"
+        enter="transition ease-out duration-500"
         enterFrom="opacity-0 translate-y-1"
         enterTo="opacity-100 translate-y-0"
-        leave="transition ease-in duration-150"
+        leave="transition ease-in duration-200"
         leaveFrom="opacity-100 translate-y-0"
         leaveTo="opacity-0 translate-y-1"
       >
